62121a2e8b9bc4736053d5cc22d6a9ccb47b6724
Seth Rider <seth.rider@gmail.com>
2024-08-13 09:44:14 +0100

    stuf

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,17 +22,7 @@ return {
     },
     config = true,
     keys = {
-      { '<leader>gn', '<cmd>Neogit<cr>' },
-    },
-  },
-  {
-    'christoomey/vim-tmux-navigator',
-    keys = {
-      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+      { '<leader>gn', '<cmd>Neogit<cr>', desc = 'Git Neogit' },
     },
   },
   {
