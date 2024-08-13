2e95ea8de319830496736655412aafcdd3e4de8c
Seth Rider <seth.rider@gmail.com>
2024-08-13 09:33:12 +0100

    rip trouble again

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,43 +4,6 @@ vim.cmd [[
 
 return {
   'wuelnerdotexe/vim-astro',
-  {
-    'folke/trouble.nvim',
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = 'Trouble',
-    keys = {
-      {
-        '<leader>xx',
-        '<cmd>Trouble diagnostics toggle<cr>',
-        desc = 'Diagnostics (Trouble)',
-      },
-      {
-        '<leader>xX',
-        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-        desc = 'Buffer Diagnostics (Trouble)',
-      },
-      {
-        '<leader>cs',
-        '<cmd>Trouble symbols toggle focus=false<cr>',
-        desc = 'Symbols (Trouble)',
-      },
-      {
-        '<leader>cl',
-        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-        desc = 'LSP Definitions / references / ... (Trouble)',
-      },
-      {
-        '<leader>xL',
-        '<cmd>Trouble loclist toggle<cr>',
-        desc = 'Location List (Trouble)',
-      },
-      {
-        '<leader>xQ',
-        '<cmd>Trouble qflist toggle<cr>',
-        desc = 'Quickfix List (Trouble)',
-      },
-    },
-  },
   'stevearc/overseer.nvim',
   {
     'stevearc/oil.nvim',
@@ -54,11 +17,13 @@ return {
     'NeogitOrg/neogit',
     dependencies = {
       'nvim-lua/plenary.nvim',
-
       'sindrets/diffview.nvim',
       'nvim-telescope/telescope.nvim',
     },
     config = true,
+    keys = {
+      { '<leader>gn', '<cmd>Neogit<cr>' },
+    },
   },
   {
     'christoomey/vim-tmux-navigator',
