452d0cfd7f9a7e455bc983d258cba5a9c2f4a21c
Seth Rider <seth.rider@gmail.com>
2024-10-10 07:04:35 +0100

    stuf

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,39 @@ vim.cmd [[
 ]]
 
 return {
+  {
+    'ej-shafran/compile-mode.nvim',
+    tag = 'v5.2.0',
+    -- you can just use the latest version:
+    -- branch = "latest",
+    -- or the most up-to-date updates:
+    -- branch = "nightly",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- if you want to enable coloring of ANSI escape codes in
+      -- compilation output, add:
+      -- { "m00qek/baleia.nvim", tag = "v1.3.0" },
+    },
+    config = function()
+      ---@type CompileModeOpts
+      vim.g.compile_mode = {
+        -- to add ANSI escape code support, add:
+        -- baleia_setup = true,
+      }
+    end,
+  },
+  {
+    'toppair/peek.nvim',
+    event = { 'VeryLazy' },
+    build = 'deno task --quiet build:fast',
+    config = function()
+      require('peek').setup {
+        app = 'browser',
+      }
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  },
   -- {
   --   'frankroeder/parrot.nvim',
   --   tag = 'v0.5.0',
