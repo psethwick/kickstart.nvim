a9f0ae40d8580ce0257b787d7b8832685745fc27
Seth Rider <seth.rider@gmail.com>
2024-10-10 07:20:05 +0100

    fix C-p doing (not what I want) in oil.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -77,9 +77,31 @@ return {
   {
     'stevearc/oil.nvim',
     lazy = false,
-    opts = {},
+    opts = {
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+        ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+        ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
+        ['<C-v>'] = 'actions.preview', -- was <C-p> and used to upset me
+        ['<C-c>'] = 'actions.close',
+        ['<C-l>'] = 'actions.refresh',
+        ['-'] = 'actions.parent',
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory', mode = 'n' },
+        ['gs'] = 'actions.change_sort',
+        ['gx'] = 'actions.open_external',
+        ['g.'] = 'actions.toggle_hidden',
+      },
+      -- Set to false to disable all of the above keymaps
+      use_default_keymaps = false,
+      view_options = {
+        show_hidden = true,
+      },
+    },
     keys = {
-      -- TODO: stop this stealing C-P
       { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
     },
   },
