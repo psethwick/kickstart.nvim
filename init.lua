cb44deb83133e06291d288e718b75e702f20dbb0
Seth Rider <seth.rider@gmail.com>
2025-03-01 13:58:19 +0000

    mini.files goes to current buffer file

--- init.lua
+++ init.lua
@@ -741,7 +741,7 @@ require('lazy').setup({
       require('mini.files').setup()
 
       vim.keymap.set('n', '-', function()
-        MiniFiles.open()
+        MiniFiles.open(vim.api.nvim_buf_get_name(0))
       end, { desc = 'open mini.files' })
 
       -- Simple and easy statusline.
