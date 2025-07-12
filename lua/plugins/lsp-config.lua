fcf4b597daf29af4f6e29205f52ec9aaa8a48595
Seth Rider <seth.rider@gmail.com>
2025-07-12 15:45:16 +0100

    lazy more things

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,7 @@
 return {
   {
     'neovim/nvim-lspconfig',
+    event = 'VeryLazy',
     dependencies = {
       { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
       'williamboman/mason-lspconfig.nvim',
