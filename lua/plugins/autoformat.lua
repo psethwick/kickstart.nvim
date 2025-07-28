cebf35d846119f006e49517d6a3101ad2f1b83db
Seth Rider <seth.rider@gmail.com>
2025-07-28 07:02:13 +0100

    neat autoformat thing

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -12,27 +12,33 @@ return {
         desc = '[F]ormat buffer',
       },
     },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true } --, cs = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+    config = function()
+      local enabled = vim.env.NO_AUTOFORMAT ~= 'true'
+
+      if enabled then
+        require('conform').setup {
+          notify_on_error = false,
+          format_on_save = function(bufnr)
+            -- Disable "format_on_save lsp_fallback" for languages that don't
+            -- have a well standardized coding style. You can add additional
+            -- languages here or re-enable it for the disabled ones.
+            local disable_filetypes = { c = true, cpp = true } --, cs = true }
+            return {
+              timeout_ms = 500,
+              lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+            }
+          end,
+          formatters_by_ft = {
+            lua = { 'stylua' },
+            python = { 'ruff_format' },
+            sql = { 'sql_formatter' },
+            javascript = { 'prettier' },
+            astro = { 'prettier' },
+            typescript = { 'prettier' },
+            typescriptreact = { 'prettier' },
+          },
         }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        python = { 'ruff_format' },
-        sql = { 'sql_formatter' },
-        javascript = { 'prettier' },
-        astro = { 'prettier' },
-        typescript = { 'prettier' },
-        typescriptreact = { 'prettier' },
-      },
-    },
+      end
+    end,
   },
 }
