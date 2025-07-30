d33eb0403f376c27c9197f1e5528ee2a0f0e711c
Seth Rider <seth.rider@gmail.com>
2025-07-30 14:31:51 +0100

    telescope -> mini.pick

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,12 +17,26 @@ return {
             vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          local lsp = require('mini.extra').pickers.lsp
+
+          map('gd', function()
+            lsp { scope = 'definition' }
+          end, '[G]oto [D]efinition')
+          map('gr', function()
+            lsp { scope = 'references' }
+          end, '[G]oto [R]eferences')
+          map('gI', function()
+            lsp { scope = 'implementation' }
+          end, '[G]oto [I]mplementation')
+          map('<leader>D', function()
+            lsp { scope = 'type_definition' }
+          end, 'Type [D]efinition')
+          map('<leader>ds', function()
+            lsp { scope = 'document_symbol' }
+          end, '[D]ocument [S]ymbols')
+          map('<leader>ws', function()
+            lsp { scope = 'workspace_symbol' }
+          end, '[W]orkspace [S]ymbols')
 
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
---
+++ lua/plugins/minipick.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    'echasnovski/mini.pick',
+    version = false,
+    dependencies = {
+      -- Optionally include icons if you use Nerd Fonts
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      -- Include mini.extra for extra pickers (like diagnostics, LSP, etc.)
+      { 'echasnovski/mini.extra' },
+    },
+    config = function()
+      require('mini.pick').setup()
+      require('mini.extra').setup()
+
+      local pickers = require('mini.extra').pickers
+
+      vim.keymap.set('n', '<leader>sf', function()
+        require('mini.pick').builtin.files()
+      end, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<C-p>', function()
+        require('mini.pick').builtin.files { tool = 'rg', extra_args = { '--files', '--hidden', '--glob', '!**/.git/*' } }
+      end, { desc = 'Find files (fd/rg)' })
+      vim.keymap.set('n', '<leader>sb', function()
+        require('mini.pick').builtin.buffer_lines()
+      end, { desc = '[S]earch [B]uffer' })
+      vim.keymap.set('n', '<leader><leader>', function()
+        require('mini.pick').builtin.buffers()
+      end, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>ss', function()
+        require('mini.pick').builtin()
+      end, { desc = '[S]elect Picker' })
+
+      vim.keymap.set('n', '<leader>sw', function()
+        require('mini.pick').builtin.grep_word()
+      end, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', function()
+        require('mini.pick').builtin.grep_live()
+      end, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>s/', function()
+        require('mini.pick').builtin.grep_live { only_open = true, prompt_title = 'Live Grep in Open Files' }
+      end, { desc = '[S]earch [/] in Open Files' })
+      vim.keymap.set('n', '<leader>sd', function()
+        pickers.diagnostic { scope = 'all' }
+      end, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sh', function()
+        require('mini.pick').builtin.help()
+      end, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', function()
+        require('mini.pick').builtin.keymaps()
+      end, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sn', function()
+        require('mini.pick').builtin.files { cwd = vim.fn.stdpath 'config' }
+      end, { desc = '[S]earch [N]eovim files' })
+      vim.keymap.set('n', '<leader>sr', function()
+        require('mini.pick').builtin.resume()
+      end, { desc = '[S]earch [R]esume' }) -- or just closes and resumes pickers
+      vim.keymap.set('n', '<leader>s.', function()
+        require('mini.pick').builtin.oldfiles()
+      end, { desc = '[S]earch Recent Files ("." for repeat)' })
+    end,
+  },
+}
