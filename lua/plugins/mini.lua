eee778736079e81569decf97a47eac5ce75f9d40
Seth Rider <seth.rider@gmail.com>
2025-08-04 14:32:08 +0100

    removing broken mini.picks

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -72,24 +72,12 @@ return {
       vim.keymap.set('n', '<leader>s/', function()
         require('mini.pick').builtin.grep_live { only_open = true, prompt_title = 'Live Grep in Open Files' }
       end, { desc = '[S]earch [/] in Open Files' })
-      vim.keymap.set('n', '<leader>sd', function()
-        pickers.diagnostic { scope = 'all' }
-      end, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sh', function()
         require('mini.pick').builtin.help()
       end, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', function()
-        require('mini.pick').builtin.keymaps()
+        require('mini.extra').pickers.keymaps()
       end, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sn', function()
-        require('mini.pick').builtin.files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
-      vim.keymap.set('n', '<leader>sr', function()
-        require('mini.pick').builtin.resume()
-      end, { desc = '[S]earch [R]esume' }) -- or just closes and resumes pickers
-      vim.keymap.set('n', '<leader>s.', function()
-        require('mini.pick').builtin.oldfiles()
-      end, { desc = '[S]earch Recent Files ("." for repeat)' })
     end,
   },
 }
