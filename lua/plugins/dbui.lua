fc8a3bc2916ce9c1afe4a59e8bac303e857fef0e
Seth Rider <seth.rider@gmail.com>
2025-08-05 21:52:50 +0100

    db keymap

--- lua/plugins/dbui.lua
+++ lua/plugins/dbui.lua
@@ -7,12 +7,10 @@ return {
     },
     cmd = {
       'DBUI',
-      'DBUIToggle',
-      'DBUIAddConnection',
-      'DBUIFindBuffer',
     },
     init = function()
       vim.g.db_ui_use_nerd_fonts = 1
+      vim.keymap.set('n', '<leader>db', ':DBUI<CR>', { desc = 'DBUI' })
     end,
   },
 }
