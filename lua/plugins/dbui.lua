cddc43ec20b0a2b12a17e76e3e76d9cf756ac286
Seth Rider <seth.rider@gmail.com>
2025-08-06 13:40:29 +0100

    maybe just don't put dadbod completion on plain sql files

--- lua/plugins/dbui.lua
+++ lua/plugins/dbui.lua
@@ -1,13 +1,8 @@
 return {
   {
     'kristijanhusak/vim-dadbod-ui',
-    dependencies = {
-      { 'tpope/vim-dadbod' },
-      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-    },
-    cmd = {
-      'DBUI',
-    },
+    dependencies = { 'tpope/vim-dadbod', 'kristijanhusak/vim-dadbod-completion' },
+    cmd = { 'DBUI' },
     init = function()
       vim.g.db_ui_use_nerd_fonts = 1
       vim.keymap.set('n', '<leader>db', ':DBUI<CR>', { desc = '[D]ata[b]ase UI' })
