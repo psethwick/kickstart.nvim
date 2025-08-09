b8f6ec77860d25c630ef5426976c93b669b51a71
Seth Rider <seth.rider@gmail.com>
2025-08-09 21:40:48 +0100

    maybe I don't need a which-key

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -46,55 +46,8 @@ return {
       require('mini.pick').setup()
       require('mini.extra').setup()
 
-      local miniclue = require 'mini.clue'
-      miniclue.setup {
-        triggers = {
-          -- Leader triggers
-          { mode = 'n', keys = '<Leader>' },
-          { mode = 'x', keys = '<Leader>' },
-
-          -- Built-in completion
-          { mode = 'i', keys = '<C-x>' },
-
-          -- `g` key
-          { mode = 'n', keys = 'g' },
-          { mode = 'x', keys = 'g' },
-
-          -- Marks
-          { mode = 'n', keys = "'" },
-          { mode = 'n', keys = '`' },
-          { mode = 'x', keys = "'" },
-          { mode = 'x', keys = '`' },
-
-          -- Registers
-          { mode = 'n', keys = '"' },
-          { mode = 'x', keys = '"' },
-          { mode = 'i', keys = '<C-r>' },
-          { mode = 'c', keys = '<C-r>' },
-
-          -- Window commands
-          { mode = 'n', keys = '<C-w>' },
-
-          -- `z` key
-          { mode = 'n', keys = 'z' },
-          { mode = 'x', keys = 'z' },
-        },
-
-        clues = {
-          miniclue.gen_clues.builtin_completion(),
-          miniclue.gen_clues.g(),
-          miniclue.gen_clues.marks(),
-          miniclue.gen_clues.registers(),
-          miniclue.gen_clues.windows(),
-          miniclue.gen_clues.z(),
-        },
-        window = {
-          delay = 500,
-        },
-      }
-
       vim.keymap.set('n', '<C-p>', function()
-        require('mini.pick').builtin.files { tool = 'rg', extra_args = { '--files', '--hidden', '--glob', '!**/.git/*' } }
+        require('mini.pick').builtin.files()
       end, { desc = 'Find files (fd/rg)' })
       vim.keymap.set('n', '<leader><leader>', function()
         require('mini.pick').builtin.buffers()
