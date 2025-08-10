e9ff01a23bc0d09bb7e41f89781992c93c0e5bab
Seth Rider <seth.rider@gmail.com>
2025-08-10 12:58:35 +0100

    remove ruby stuf

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,9 +8,8 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true },
     },
     config = function(_, opts)
       require('nvim-treesitter.install').prefer_git = true
