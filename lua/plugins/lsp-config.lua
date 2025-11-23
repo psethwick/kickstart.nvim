ceb40ef00731e3e0b83f8187015961e15d2f9ba0
Seth Rider <seth.rider@gmail.com>
2025-11-23 10:09:24 +0000

    lsp works if I start nvim <file>, now

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,7 +1,7 @@
 return {
   {
     'neovim/nvim-lspconfig',
-    event = 'VeryLazy',
+    event = 'BufReadPost',
     dependencies = {
       { 'j-hui/fidget.nvim', opts = {} },
       { 'folke/neodev.nvim', opts = {} },
