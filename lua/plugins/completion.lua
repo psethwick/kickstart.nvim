0e036f868446d5155677ac26ff34e68f96b543f3
Seth Rider <seth.rider@gmail.com>
2026-01-12 09:13:34 +0000

    wip trying dbee

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,10 +1,27 @@
 return {
   {
+    'saghen/blink.compat',
+    -- use v2.* for blink.cmp v1.*
+    version = '2.*',
+    lazy = true,
+    opts = {},
+  },
+  {
     'saghen/blink.cmp',
+
     -- optional: provides snippets for the snippet source
     -- use a release tag to download pre-built binaries
     version = '1.*',
-
+    dependencies = {
+      {
+        'mattiasmts/cmp-dbee',
+        dependencies = {
+          { 'kndndrj/nvim-dbee' },
+        },
+        ft = 'sql',
+        opts = {},
+      },
+    },
     opts = {
       -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
       keymap = { preset = 'default' },
@@ -16,13 +33,14 @@ return {
       completion = { documentation = { auto_show = false } },
 
       sources = {
-        default = { 'lsp', 'path', 'buffer' },
-        per_filetype = {
-          sql = { 'snippets', 'dadbod', 'buffer' },
-        },
-        providers = {
-          dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
-        },
+        default = { 'lsp', 'path', 'buffer', 'cmp-dbee' },
+        { 'cmp-dbee' },
+        -- per_filetype = {
+        --   sql = { 'snippets', 'dadbod', 'buffer' },
+        -- },
+        -- providers = {
+        --   dadbod = { name = 'Dadbod', module = 'vim_dadbod_completion.blink' },
+        -- },
       },
 
       fuzzy = { implementation = 'prefer_rust_with_warning' },
--- lua/plugins/dbui.lua
+++ lua/plugins/dbui.lua
@@ -1,11 +1,18 @@
 return {
   {
-    'kristijanhusak/vim-dadbod-ui',
-    dependencies = { 'tpope/vim-dadbod', 'kristijanhusak/vim-dadbod-completion' },
-    cmd = { 'DBUI' },
+    'kndndrj/nvim-dbee',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+    build = function()
+      require('dbee').install()
+    end,
+    cmd = { 'Dbee' },
     init = function()
-      vim.g.db_ui_use_nerd_fonts = 1
-      vim.keymap.set('n', '<leader>db', ':DBUI<CR>', { desc = '[D]ata[b]ase UI' })
+      vim.keymap.set('n', '<leader>db', ':Dbee<CR>', { desc = '[D]ata[b]ase UI' })
+    end,
+    config = function()
+      require('dbee').setup()
     end,
   },
 }
