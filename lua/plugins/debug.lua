8e631c023a52f38c96d2bab640ff102f93be087f
Seth Rider <seth.rider@gmail.com>
2026-01-12 08:19:36 +0000

    lets try again wo dapui

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -2,7 +2,7 @@ return {
   'mfussenegger/nvim-dap',
 
   dependencies = {
-    'rcarriga/nvim-dap-ui',
+    -- 'rcarriga/nvim-dap-ui',
     'nvim-neotest/nvim-nio',
     'theHamsta/nvim-dap-virtual-text',
 
@@ -14,11 +14,41 @@ return {
     'stevearc/overseer.nvim',
   },
   keys = {
-    { '<F5>', function() require('dap').continue() end, desc = 'Debug: Start/Continue' },
-    { '<F1>', function() require('dap').step_into() end, desc = 'Debug: Step Into' },
-    { '<F2>', function() require('dap').step_over() end, desc = 'Debug: Step Over' },
-    { '<F3>', function() require('dap').step_out() end, desc = 'Debug: Step Out' },
-    { '<leader>b', function() require('dap').toggle_breakpoint() end, desc = 'Debug: Toggle Breakpoint' },
+    {
+      '<F5>',
+      function()
+        require('dap').continue()
+      end,
+      desc = 'Debug: Start/Continue',
+    },
+    {
+      '<F1>',
+      function()
+        require('dap').step_into()
+      end,
+      desc = 'Debug: Step Into',
+    },
+    {
+      '<F2>',
+      function()
+        require('dap').step_over()
+      end,
+      desc = 'Debug: Step Over',
+    },
+    {
+      '<F3>',
+      function()
+        require('dap').step_out()
+      end,
+      desc = 'Debug: Step Out',
+    },
+    {
+      '<leader>b',
+      function()
+        require('dap').toggle_breakpoint()
+      end,
+      desc = 'Debug: Toggle Breakpoint',
+    },
     {
       '<leader>B',
       function()
@@ -26,11 +56,17 @@ return {
       end,
       desc = 'Debug: Set Breakpoint',
     },
-    { '<F7>', function() require('dapui').toggle() end, desc = 'Debug: See last session result.' },
+    -- {
+    --   '<F7>',
+    --   function()
+    --     require('dapui').toggle()
+    --   end,
+    --   desc = 'Debug: See last session result.',
+    -- },
   },
   config = function()
     local dap = require 'dap'
-    local dapui = require 'dapui'
+    -- local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
       automatic_installation = true,
@@ -45,29 +81,29 @@ return {
 
     require('nvim-dap-virtual-text').setup {}
 
-    dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      controls = {
-        icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
-        },
-      },
-    }
+    -- dapui.setup {
+    --   -- Set icons to characters that are more likely to work in every terminal.
+    --   --    Feel free to remove or use ones that you like more! :)
+    --   --    Don't feel like these are good choices.
+    --   icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+    --   controls = {
+    --     icons = {
+    --       pause = '⏸',
+    --       play = '▶',
+    --       step_into = '⏎',
+    --       step_over = '⏭',
+    --       step_out = '⏮',
+    --       step_back = 'b',
+    --       run_last = '▶▶',
+    --       terminate = '⏹',
+    --       disconnect = '⏏',
+    --     },
+    --   },
+    -- }
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+    -- dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    -- dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    -- dap.listeners.before.event_exited['dapui_config'] = dapui.close
     dap.adapters.lldb = {
       type = 'executable',
       command = '/sbin/lldb-vscode',
