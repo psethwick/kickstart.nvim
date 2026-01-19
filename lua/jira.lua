b487649f1912a2ce22f6e6567bc8a4c2cfcc3441
Seth Rider <seth.rider@gmail.com>
2026-01-19 07:47:03 +0000

    amend jira query

--- lua/jira.lua
+++ lua/jira.lua
@@ -1,7 +1,7 @@
 local function insert_jira_ticket()
   local db_path = '~/.local/share/pstore/pstore.db'
   local query =
-    [[ "select w.id, w.state, w.title from work w join person p on p.id = w.assigned_to_id where p.name = 'Seth Rider' and w.state not in ('Delivered','Done','Deployed');" ]]
+    [[ "select w.id, w.state, w.title, p.name from work w left join person p on p.id = w.assigned_to_id where (p.name = 'Seth Rider' or w.id like 'ST%') and w.state not in ('Delivered','Done','Deployed');" ]]
 
   local jira_cmd = string.format('sqlite3 %s -csv %s', db_path, query)
 
@@ -30,11 +30,12 @@ local function insert_jira_ticket()
       local ticket_id = fields[1]
       local status = fields[2] or ''
       local summary = fields[3] or ''
+      local assigned = fields[4] or ''
 
       if ticket_id and ticket_id:match '^[A-Z]+%-[0-9]+$' then
         table.insert(tickets, {
           id = ticket_id,
-          display = string.format('%s [%s] %s', ticket_id, status, summary),
+          display = string.format('%s [%s] %s %s', ticket_id, status, summary, assigned),
         })
       end
     end
