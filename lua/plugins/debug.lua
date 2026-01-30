4ec3baf9c40b755f528afeb0905912149088c1a3
Seth Rider <seth.rider@gmail.com>
2026-01-30 14:05:04 +0000

    tidy debug.lua + maybe pytest bdd

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -2,19 +2,12 @@ return {
   {
     'igorlfs/nvim-dap-view',
     opts = {},
-    -- keys = {
-    --   'F7',
-    --   function()
-    --     require('dap-view').toggle()
-    --   end,
-    -- },
   },
   {
     'mfussenegger/nvim-dap',
 
     dependencies = {
       'nvim-neotest/nvim-nio',
-      -- 'theHamsta/nvim-dap-virtual-text',
 
       'williamboman/mason.nvim',
       'jay-babu/mason-nvim-dap.nvim',
@@ -66,17 +59,9 @@ return {
         end,
         desc = 'Debug: Set Breakpoint',
       },
-      -- {
-      --   '<F7>',
-      --   function()
-      --     require('dapui').toggle()
-      --   end,
-      --   desc = 'Debug: See last session result.',
-      -- },
     },
     config = function()
       local dap = require 'dap'
-      -- local dapui = require 'dapui'
 
       require('mason-nvim-dap').setup {
         automatic_installation = true,
@@ -88,8 +73,6 @@ return {
           'delve',
         },
       }
-
-      -- require('nvim-dap-virtual-text').setup {}
 
       dap.adapters.lldb = {
         type = 'executable',
@@ -126,6 +109,67 @@ return {
       })
       require('dap-go').setup()
       require('dap-python').setup 'python'
+
+      dap.listeners.after.event.initialized['dap-view-toggle'] = function()
+        require('dap-view').toggle()
+      end
+
+      dap.listeners.before.event.terminated['dap-view-toggle'] = function()
+        require('dap-view').toggle()
+      end
+
+      local function debug_pytest_bdd_picker()
+        local pick = require 'mini.pick'
+
+        -- Use pytest to collect tests.
+        -- We use 'args' to ensure we only get the test IDs
+        local cmd = 'pytest -q --collect-only --nd --color=no'
+
+        -- Run the command and capture output
+        local handle = io.popen(cmd)
+        if not handle then
+          return
+        end
+        local result = handle:read '*a'
+        handle:close()
+
+        local items = {}
+        for line in result:gmatch '[^\r\n]+' do
+          -- Clean up the pytest output to get clean scenario/test names
+          if not line:match 'collected' and line ~= '' then
+            table.insert(items, line)
+          end
+        end
+
+        pick.start {
+          source = {
+            items = items,
+            name = 'Debug BDD Scenario',
+            choose = function(item)
+              local dap = require 'dap'
+              local dap_python = require 'dap-python'
+              -- local dapview = require('dapview') -- dapview is already handled by global listeners
+
+              -- 1. Setup a listener: When the session starts, open dapview
+              -- This is handled by global listeners now, so we remove the redundant local listener
+              -- dap.listeners.after.event_initialized["dapview_config"] = function()
+              --   dapview.open()
+              -- end
+
+              -- 2. Launch the debugger with the selected -k filter
+              dap_python.debug_selection {
+                module = 'pytest',
+                args = { '-k', item },
+                -- Ensure Django env is loaded if your shell doesn't do it automatically
+                -- env = { DJANGO_SETTINGS_MODULE = "my_project.settings" },
+                console = 'integratedTerminal',
+              }
+            end,
+          },
+        }
+      end
+
+      vim.keymap.set('n', '<leader>db', debug_pytest_bdd_picker, { desc = 'Fuzzy debug BDD with dapview' })
     end,
   },
 }
